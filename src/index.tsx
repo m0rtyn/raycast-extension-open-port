ad2d2ef30e99ccf25408f1bd7caa946e72879984
Martyn <zogacc@gmail.com>
2023-07-23 18:49:10 +0400

    init working commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,16 @@
-import { Detail } from "@raycast/api";
+import { ActionPanel, Detail, Action } from "@raycast/api";
 
 export default function Command() {
-  return <Detail markdown="# Hello World" />;
+  const port = process.argv.slice(2)?.[0] ?? 3000
+  const url = `https://localhost:${port}`
+  return (
+    <Detail
+      markdown="# Join the gang!"
+      actions={
+        <ActionPanel>
+          <Action.OpenInBrowser url={url} />
+        </ActionPanel>
+      }
+    />
+  );
 }
